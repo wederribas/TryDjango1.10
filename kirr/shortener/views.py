dd507a33ac437908059368f1ab01f03cbb56a185
Weder Ribas <wederribas@gmail.com>
2017-03-05 12:09:42 -0300

    Delete .pyc files / Add two new views for testing

--- kirr/shortener/views.py
+++ kirr/shortener/views.py
@@ -1,3 +1,10 @@
+from django.http import HttpResponse
 from django.shortcuts import render
+from django.views import View
 
-# Create your views here.
+def kirr_redirect_view(request, *args, **kwargs):
+	return HttpResponse("Function based view")
+
+class KirrCBView(View):
+	def get(self, request, *args, **kwargs):
+		return HttpResponse("Class based view")
